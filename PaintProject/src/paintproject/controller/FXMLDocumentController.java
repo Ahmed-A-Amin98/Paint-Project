bcfbd98970643d6c83d9a53d7f42e1477d771b93
hp\hp_pc <ahmedabdelhamid106@gmail.com>
2018-04-10 20:26:46 +0200

    s

--- PaintProject/src/paintproject/controller/FXMLDocumentController.java
+++ PaintProject/src/paintproject/controller/FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paintproject.controller;
 
 import java.net.URL;
@@ -12,10 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author hp_pc
- */
+
 public class FXMLDocumentController implements Initializable {
 
 @FXML
