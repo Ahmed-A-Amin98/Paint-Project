6237ca48bb35903ec65a36073f37e33a2326bf75
hp\hp_pc <ahmedabdelhamid106@gmail.com>
2018-04-10 20:25:23 +0200

    s

--- PaintProject/src/paintproject/model/Ellipse.java
+++ PaintProject/src/paintproject/model/Ellipse.java
@@ -2,6 +2,10 @@
 package paintproject.model;
 
 
-public class Ellipse {
+public class Ellipse extends AbstractShape implements Shape,Cloneable {
+    
+public Ellipse(int x, int y) {
+super(x, y);
+}
     
 }
